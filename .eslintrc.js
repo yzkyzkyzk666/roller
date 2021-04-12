87c9a06fed429673b74f8aa870930c06fb246cc7
exis <gotexis@gmail.com>
2021-04-12 17:51:16 +1000

    use new template

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module", // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true // Allows for the parsing of JSX
+    }
+  },
+  settings: {
+    react: {
+      version: "detect" // Tells eslint-plugin-react to automatically detect the version of React to use
+    }
+  },
+  extends: [
+    "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    "prettier", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    "plugin:prettier/recommended"
+  ],
+  rules: {
+    "prettier/prettier": [
+      "warn",
+      {
+        printWidth: 120,
+        tabWidth: 2,
+        "editor.formatOnSave": true,
+        arrowParens: "always",
+        semi: false,
+        jsxSingleQuote: false,
+        trailingComma: "none",
+        singleQuote: false
+      }
+    ],
+    "@typescript-eslint/explicit-module-boundary-types": 0
+  },
+}
